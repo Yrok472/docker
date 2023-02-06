34625ee55c21a3041df40e446aea69dae7e77990
Yury Sagalaev <yrok472@gmail.com>
2023-02-06 03:00:42 +0300

    update to 25.0.3

--- 25/fpm-alpine/build_fpm-alpine_image.sh
+++ 25/fpm-alpine/build_fpm-alpine_image.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# "cd" into "25.0.2-fpm-alpine" before script running
+# "cd" into "docker/25/fpm-alpine/" before script running
 
 # Preconditions:
 # docker login https://harbor.smartvz.site # enter creds from https://harbor.smartvz.site
 
-docker image rm infra/25.0.2-fpm-alpine
-docker image rm 25.0.2-fpm-alpine
-docker build . -t 25.0.2-fpm-alpine:latest
-docker tag 25.0.2-fpm-alpine harbor.smartvz.site/infra/25.0.2-fpm-alpine:latest
-docker push harbor.smartvz.site/infra/25.0.2-fpm-alpine:latest
+docker image rm infra/25.0.3-fpm-alpine
+docker image rm 25.0.3-fpm-alpine
+docker build . -t 25.0.3-fpm-alpine:latest --no-cache
+docker tag 25.0.3-fpm-alpine harbor.smartvz.site/infra/25.0.3-fpm-alpine:latest
+docker push harbor.smartvz.site/infra/25.0.3-fpm-alpine:latest
